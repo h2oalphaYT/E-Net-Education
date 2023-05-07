0f6e14c6e1f089e34ce448f9417b11ea0df9f4b3
Chanuka Devin <chanukaalpha@gmail.com>
2023-05-07 20:11:30 +0530

    update

--- E-Net-Education/src/main/java/com/oop/servlet/DeleteProfileservlet.java
+++ E-Net-Education/src/main/java/com/oop/servlet/DeleteProfileservlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 public class DeleteProfileservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
